57205f8df728e5e2abbf37153ce19969ea7efbf7
gfenelus <gfenelus@student.neumont.edu>
2020-08-21 16:18:56 -0600

    test commit

--- recipie.py
+++ recipie.py
@@ -1,18 +1,18 @@
 from flask import Flask  # import flask
 
-app = Flask(__name__)  # create an app instance
+app = Flask(__name__)
 
 
-@app.route("/")  # at the end point /
-def hello():  # call method hello
-    return f"Hello world!"  # which returns "hello world"
+@app.route("/")
+def hello():
+    return f"HomePage"
 
 
-@app.route("/<name>")  # at the end point /<name>
-def hello_name(name):  # call method hello_name
-    return "Hello, " + name  # which returns "hello + name
+@app.route("/<name>")
+def hello_name(name):
+    return "Hello, " + name
 
 
-if __name__ == "__main__":  # on running python app.py
+if __name__ == "__main__":
     # app.run()  # run the flask app
     app.run(host='0.0.0.0', debug=True)
