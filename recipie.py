ee1cd5151ce59ddf56961274f46ffc49b22b2953
gfenelus <gfenelus@student.neumont.edu>
2020-08-21 17:33:47 -0600

    printed user object to localhost

--- recipie.py
+++ recipie.py
@@ -1,4 +1,5 @@
 from flask import Flask  # import flask
+from user import *
 
 app = Flask(__name__)
 
@@ -11,6 +12,30 @@ def hello():
 @app.route("/<name>")
 def hello_name(name):
     return "Hello, " + name
+
+
+@app.route("/showUser")
+def show_user():
+    recipieDict1 = {
+        "name": "Fried Eggs",
+        "ingredients": ["eggs", "oil", "salt", "pepper"],
+        "time": 15  # in minuets
+
+    }
+    recipieDict2 = {
+        "name": "Cereal",
+        "ingredients": ["cereal", "milk", "bowl", "spoon"],
+        "time": 2  # in minuets
+
+    }
+    u = User()
+    u.firstName = "Bob"
+    u.lastName = "Smith"
+    u.id = 1
+    u.recipes.append(recipieDict1)
+    u.recipes.append(recipieDict2)
+
+    return u.show_data()
 
 
 if __name__ == "__main__":
--- user.py
+++ user.py
@@ -1,8 +1,22 @@
+def parse_recipes(recipieList):
+    recipe_string = ""
+    for recipeDict in recipieList:
+        name = recipeDict["name"]
+        ingredients = recipeDict["ingredients"]
+        time = recipeDict["time"]
+        recipe_string += f"{name},\r\n Ingredients: {ingredients}, \r\nReady in {time} minuets\r\r"
+    return recipe_string
+
+
 class User:
 
     def __init__(self):
         self.id = None
         self.firstName = None
         self.lastName = None
+        self.recipie = {}
         self.recipes = []
-        self.timeZone = None
+
+    def show_data(self):
+        user_data = f"{self.firstName} {self.lastName}, Recipes: {parse_recipes(self.recipes)}  "
+        return user_data
